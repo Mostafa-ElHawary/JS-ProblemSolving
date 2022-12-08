82a9484a375c0d41cd03f7b110cad2b7e0f8d0a1
Azzi-Mo <alhwarymostafa@gmail.com>
2022-12-08 05:14:46 +0200

    upload2problems

---
+++ Opposite number/Oppositenumber.js
@@ -0,0 +1,14 @@
+// DESCRIPTION:
+// Very simple, given an integer or a floating-point number, find its opposite.
+
+// Examples:
+
+// 1: -1
+// 14: -14
+// -34: 34
+
+function Func(n)
+{
+   return -n 
+}
+console.log(Func(-1));
---
+++ ts.ts
@@ -0,0 +1,6 @@
+function Func(n: number)
+{
+   n > 0 ?  -n : n
+
+}
+console.log(Func(-1));
