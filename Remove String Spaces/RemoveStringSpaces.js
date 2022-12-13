050edcd97b7f102e4cd053ab19f7eb5dd71834e5
Azzi-Mo <alhwarymostafa@gmail.com>
2022-12-13 02:48:35 +0200

    upload2problems

---
+++ Remove String Spaces/RemoveStringSpaces.js
@@ -0,0 +1,8 @@
+// DESCRIPTION:
+// Simple, remove the spaces from the string, then return the resultant string.
+
+
+function Func(n) {
+  return n.split(" ").join("");
+}
+console.log(Func("h e el lo th ere"));
