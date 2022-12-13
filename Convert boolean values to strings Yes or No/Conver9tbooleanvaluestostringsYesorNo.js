7e688e238d843b0dd5042a3c29f7d2ae28ea2259
Azzi-Mo <alhwarymostafa@gmail.com>
2022-12-13 03:17:32 +0200

    upload2problems0

---
+++ Convert boolean values to strings Yes or No/Conver9tbooleanvaluestostringsYesorNo.js
@@ -0,0 +1,5 @@
+// DESCRIPTION:
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+let Func = (n) => n ? 'Yes' : 'No'
+console.log(Func(false))
